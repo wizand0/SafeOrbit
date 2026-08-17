14867c863b8ec0fa68e022aa536a8e435de429c9
wizand0 <xabor@yandex.ru>
2026-08-17 16:36:48 +0300

    build: fix corrupted manifestPlaceholders value and remove leftover Agora artifacts

--- app/src/main/java/ru/wizand/safeorbit/presentation/client/ServerDetailsActivity.kt
+++ app/src/main/java/ru/wizand/safeorbit/presentation/client/ServerDetailsActivity.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.location.Geocoder
 import android.net.Uri
+import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.viewModels
